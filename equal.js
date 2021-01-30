d4b7fc36e264e5b1af62452bb18e4b456bacf353
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-01-29 17:33:01 -0800

    add equal question

---
+++ equal.js
@@ -0,0 +1,12 @@
+// Common Javascript Interview Question
+
+// What is the difference between "==" and "==="?
+
+// == Compares the values
+// === Compares the values and the type
+
+console.log(5 == "5")
+// true
+
+console.log(5 === "5")
+// false
